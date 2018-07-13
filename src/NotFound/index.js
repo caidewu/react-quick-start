677be3f70c2be61258ccf34dda00108288736f21
caidewu <caidewu555@gmail.com>
2018-07-13 15:50:21 +0800

    add 404 page

---
+++ src/NotFound/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="container">
+      <div className="content">404</div>
+    </div>
+  );
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Hello from './Hello';
 import About from './About';
+import NotFound from './NotFound';
 
 const routes = [
   {
@@ -13,14 +14,19 @@ const routes = [
     path: '/about',
     component: About,
   },
+  {
+    component: NotFound,
+  }
 ];
 
 export default () => {
   return (<div>
     <Link to="/">index</Link> | <Link to="/about">about</Link>
+    <Switch>
     {routes.map((route, index) => (
       <Route key={index} path={route.path} exact={route.exact} component={route.component} />
     ))}
+    </Switch>
   </div>);
 }
 
