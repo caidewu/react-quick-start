164863e4e8d0c43bb764804651fea15af4748678
caidewu <caidewu@xinlebao.com.cn>
2018-07-17 10:18:48 +0800

    添加antd

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Hello from './pages/Hello';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Motion from './pages/Motion';
+import Antd from './pages/Antd';
 
 const routes = [
   {
@@ -18,6 +19,10 @@ const routes = [
   {
     path: '/motion',
     component: Motion,
+  },
+  {
+      path: '/antd',
+      component: Antd,
   },
   {
     component: NotFound,
