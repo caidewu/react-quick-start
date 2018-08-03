ebc25fe43e55636d5baae5f486f67fa4d3e0379a
蔡德伍 <caidewu555@gmail.com>
2018-08-03 15:48:03 +0800

    Revert "使用webpack构建"

--- build/client.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-require('eventsource-polyfill');
-const hotClient = require('webpack-hot-middleware/client?http://localhost:5000&reload=true');
-
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-});
